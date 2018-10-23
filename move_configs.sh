bd99bf839940b0ce7691ee0820f308e3108e2bcc
Kirill Smirnov <smirnov.kirill.vladimirovich@gmail.com>
2018-10-23 19:13:27 +0300

    configs

---
+++ move_configs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp desktop.jpg /usr/share/lxde/wallpapers/
+cp desktop-items-0.conf /home/kirill/.config/lxpanel/LXDE/panels/
+cp lxde-rc.xml /home/kirill/.config/openbox/
+cp lxdm.conf /etc/lxdm/
+cp panel /home/kirill/.config/lxpanel/LXDE/panels/
