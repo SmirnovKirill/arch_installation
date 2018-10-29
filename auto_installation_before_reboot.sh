42bea38259b8cf4924f7fa3d7b23af939ab6dbca
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-29 17:55:06 +0300

    Update auto_installation_before_reboot.sh

--- auto_installation_before_reboot.sh
+++ auto_installation_before_reboot.sh
@@ -20,14 +20,14 @@ if [[ $WINDOWS_INSTALLED == "y" ]]; then
   HINTS_STRING="$(grub-probe --target=hints_string /efi/EFI/Microsoft/Boot/bootmgfw.efi)"
 
   cat << EndOfText >> /boot/grub/custom.cfg
-  menuentry "Microsoft Windows 10" {
-    insmod part_gpt
-    insmod fat
-    insmod search_fs_uuid
-    insmod chain
-    search --fs-uuid --set=root $HINTS_STRING $FS_UUSID
-    chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-  }
+menuentry "Microsoft Windows 10" {
+  insmod part_gpt
+  insmod fat
+  insmod search_fs_uuid
+  insmod chain
+  search --fs-uuid --set=root $HINTS_STRING $FS_UUSID
+  chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+}
 EndOfText
 fi
 
