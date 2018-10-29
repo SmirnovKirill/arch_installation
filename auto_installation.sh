27be0060e243175d03378479ce828a5d4aafde4e
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-29 16:43:59 +0300

    Update auto_installation.sh

--- auto_installation.sh
+++ auto_installation.sh
@@ -7,7 +7,7 @@ WINDOWS_INSTALLED = "y"
 #локаль, время
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 timedatectl set-ntp true
-sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf 
 
