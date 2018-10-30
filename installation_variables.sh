2208314410192af830a511db3c7361c5a7ca217d
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-30 16:36:46 +0300

    Update installation_variables.sh

--- installation_variables.sh
+++ installation_variables.sh
@@ -2,5 +2,9 @@
 
 set -ex
 
-WINDOWS_INSTALLED="y"
 USER="kirill"
+REPOSITORY_URL="https://github.com/SmirnovKirill/arch_installation.git"
+WINDOWS_INSTALLED="y"
+WINDOWS_GRUB_ENTRY_TITLE="Microsoft Windows 10"
+GIT_USER_EMAIL="smirnov.kirill.vladimirovich@gmail.com"
+GIT_USER_NAME="Kirill Smirnov"
