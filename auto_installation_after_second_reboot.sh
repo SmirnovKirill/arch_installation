6474efb9d55566608664cdcf4218105788c4f789
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-30 16:26:53 +0300

    Update auto_installation_after_second_reboot.sh

--- auto_installation_after_second_reboot.sh
+++ auto_installation_after_second_reboot.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+set -ex
+
+CURRENT_DIRECTORY="$(dirname "$0")"
+source "$CURRENT_DIRECTORY/installation_variables.sh"
+
 sudo -u kirill cp "$CURRENT_DIRECTORY/resources/desktop.jpg" "/usr/share/lxde/wallpapers/desktop.jpg"
 sudo -u kirill cp "$CURRENT_DIRECTORY/configs/lxdm.conf" "/etc/lxdm/lxdm.conf"
 sudo -u kirill cp "$CURRENT_DIRECTORY/configs/panel" "/home/$USER/.config/lxpanel/LXDE/panels/panel"
