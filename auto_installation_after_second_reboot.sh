0e6deb5f70a15b7c5d57109401e6c157b04f49b4
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-30 15:48:33 +0300

    Create auto_installation_after_second_reboot.sh

---
+++ auto_installation_after_second_reboot.sh
@@ -0,0 +1,5 @@
+sudo -u kirill cp "$CURRENT_DIRECTORY/resources/desktop.jpg" "/usr/share/lxde/wallpapers/desktop.jpg"
+sudo -u kirill cp "$CURRENT_DIRECTORY/configs/lxdm.conf" "/etc/lxdm/lxdm.conf"
+sudo -u kirill cp "$CURRENT_DIRECTORY/configs/panel" "/home/$USER/.config/lxpanel/LXDE/panels/panel"
+sudo -u kirill cp "$CURRENT_DIRECTORY/configs/desktop-items-0.conf" "/home/$USER/.config/lxpanel/LXDE/panels/desktop-items-0.conf"
+sudo -u kirill cp "$CURRENT_DIRECTORY/configs/lxde-rc.xml" "/home/$USER/.config/openbox/lxde-rc.xml"
