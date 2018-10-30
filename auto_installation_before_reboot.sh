d0a2efd8bfd5e02035a57d21496b934866d6b79c
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-30 14:20:37 +0300

    Update auto_installation_before_reboot.sh

--- auto_installation_before_reboot.sh
+++ auto_installation_before_reboot.sh
@@ -7,7 +7,6 @@ source "$CURRENT_DIRECTORY/installation_variables.sh"
 
 #локаль, время
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-timedatectl set-ntp true
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf 
