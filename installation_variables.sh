361b6c508ac8982db3a04a4558d2ba5508718105
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-31 18:49:42 +0300

    Update installation_variables.sh

--- installation_variables.sh
+++ installation_variables.sh
@@ -8,3 +8,5 @@ WINDOWS_INSTALLED="y"
 WINDOWS_GRUB_ENTRY_TITLE="Microsoft Windows 10"
 GIT_USER_EMAIL="smirnov.kirill.vladimirovich@gmail.com"
 GIT_USER_NAME="Kirill Smirnov"
+FONT_SIZE=16
+FONT_NAME="sans"
