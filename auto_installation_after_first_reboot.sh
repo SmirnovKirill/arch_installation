bc53dbc80268e5f68c2275af6a29635b04f0d89b
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-10-31 16:40:04 +0300

    Update auto_installation_after_first_reboot.sh

--- auto_installation_after_first_reboot.sh
+++ auto_installation_after_first_reboot.sh
@@ -12,16 +12,4 @@ sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf 
 
-echo "EDITOR=vim" >> /etc/environment
-echo "SUDO_EDITOR=vim" >> /etc/environment
-
-#программы
-#xorg-xinit для ручной инициализации иксов
-pacman -S \
-  vim \
-  xorg-server \
-  xorg-xinit \
-  openbox \
-  ttf-dejavu
-  
 localectl --no-convert set-x11-keymap "us,ru" "pc105" "" "grp:alt_shift_toggle"
