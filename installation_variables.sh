f667f6eea069f7d93c1842ae7b0c22f52d36cd96
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-11-01 12:38:27 +0300

    Update installation_variables.sh

--- installation_variables.sh
+++ installation_variables.sh
@@ -12,12 +12,12 @@ FONT_SIZE=16
 FONT_NAME="sans"
 
 function substitute_variables() {
-  sed -i "s/\$USER/$USER/g" $1
-  sed -i "s/\$REPOSITORY_URL/$REPOSITORY_URL/g" $1
-  sed -i "s/\$WINDOWS_INSTALLED/$WINDOWS_INSTALLED/g" $1
-  sed -i "s/\$WINDOWS_GRUB_ENTRY_TITLE/$WINDOWS_GRUB_ENTRY_TITLE/g" $1
-  sed -i "s/\$GIT_USER_EMAIL/$GIT_USER_EMAIL/g" $1
-  sed -i "s/\$GIT_USER_NAME/$GIT_USER_NAME/g" $1
-  sed -i "s/\$FONT_SIZE/$FONT_SIZE/g" $1
-  sed -i "s/\$FONT_NAME/$FONT_NAME/g" $1
+  sudo -u $USER sed -i "s/\$USER/$USER/g" $1
+  sudo -u $USER sed -i "s/\$REPOSITORY_URL/${REPOSITORY_URL//\\/\\\\}/g" $1
+  sudo -u $USER sed -i "s/\$WINDOWS_INSTALLED/$WINDOWS_INSTALLED/g" $1
+  sudo -u $USER sed -i "s/\$WINDOWS_GRUB_ENTRY_TITLE/$WINDOWS_GRUB_ENTRY_TITLE/g" $1
+  sudo -u $USER sed -i "s/\$GIT_USER_EMAIL/$GIT_USER_EMAIL/g" $1
+  sudo -u $USER sed -i "s/\$GIT_USER_NAME/$GIT_USER_NAME/g" $1
+  sudo -u $USER sed -i "s/\$FONT_SIZE/$FONT_SIZE/g" $1
+  sudo -u $USER sed -i "s/\$FONT_NAME/$FONT_NAME/g" $1
 }
