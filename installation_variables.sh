e91f4d8960f2376ace9acac9eaf04476165a5c9b
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-11-01 17:09:33 +0300

    Update installation_variables.sh

--- installation_variables.sh
+++ installation_variables.sh
@@ -9,7 +9,8 @@ WINDOWS_GRUB_ENTRY_TITLE="Microsoft Windows 10"
 GIT_USER_EMAIL="smirnov.kirill.vladimirovich@gmail.com"
 GIT_USER_NAME="Kirill Smirnov"
 FONT_SIZE=16
-FONT_NAME="DejaVu Sans Mono Book"
+FONT_NAME="DejaVuSansCondensed"
+FONT_NAME_XTERM="DejaVuSansMono" #Xterm нужен моноширинный шрифт
 
 function substitute_variables {
   sudo -u $USER sed -i "s/\$USER/$USER/g" $1
@@ -20,4 +21,5 @@ function substitute_variables {
   sudo -u $USER sed -i "s/\$GIT_USER_NAME/$GIT_USER_NAME/g" $1
   sudo -u $USER sed -i "s/\$FONT_SIZE/$FONT_SIZE/g" $1
   sudo -u $USER sed -i "s/\$FONT_NAME/$FONT_NAME/g" $1
+  sudo -u $USER sed -i "s/\$FONT_NAME_XTERM/$FONT_NAME_XTERM/g" $1
 }
