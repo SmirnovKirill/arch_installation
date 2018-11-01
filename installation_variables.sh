2582cc9ad8f5eccc095666c3afe5564c3bad435c
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-11-01 12:10:11 +0300

    Update installation_variables.sh

--- installation_variables.sh
+++ installation_variables.sh
@@ -10,3 +10,14 @@ GIT_USER_EMAIL="smirnov.kirill.vladimirovich@gmail.com"
 GIT_USER_NAME="Kirill Smirnov"
 FONT_SIZE=16
 FONT_NAME="sans"
+
+function substituteVariables() {
+  sed -i "s/\$USER/$USER/g" $1
+  sed -i "s/\$REPOSITORY_URL/$REPOSITORY_URL/g" $1
+  sed -i "s/\$WINDOWS_INSTALLED/$WINDOWS_INSTALLED/g" $1
+  sed -i "s/\$WINDOWS_GRUB_ENTRY_TITLE/$WINDOWS_GRUB_ENTRY_TITLE/g" $1
+  sed -i "s/\$GIT_USER_EMAIL/$GIT_USER_EMAIL/g" $1
+  sed -i "s/\$GIT_USER_NAME/$GIT_USER_NAME/g" $1
+  sed -i "s/\$FONT_SIZE/$FONT_SIZE/g" $1
+  sed -i "s/\$FONT_NAME/$FONT_NAME/g" $1
+}
