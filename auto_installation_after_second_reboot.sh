ebc420a51a5bd0693decc63dd94c48f89417d945
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-11-02 16:37:50 +0300

    Delete auto_installation_after_second_reboot.sh

--- auto_installation_after_second_reboot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-CURRENT_DIRECTORY="$(dirname "$0")"
-source "$CURRENT_DIRECTORY/installation_variables.sh"
-
-sudo -u kirill cp "$CURRENT_DIRECTORY/configs/desktop-items-0.conf" "/home/$USER/.config/pcmanfm/default/desktop-items-0.conf"
