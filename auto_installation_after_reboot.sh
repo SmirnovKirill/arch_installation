a68ad7edff1f16840063722286437eff9691372c
Kirill <smirnov.kirill.vladimirovich@gmail.com>
2018-12-07 00:02:44 +0300

    Update auto_installation_after_reboot.sh

--- auto_installation_after_reboot.sh
+++ auto_installation_after_reboot.sh
@@ -11,5 +11,3 @@ timedatectl set-ntp true
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf 
-
-localectl --no-convert set-x11-keymap "us,ru" "pc105" "" "grp:alt_shift_toggle"
