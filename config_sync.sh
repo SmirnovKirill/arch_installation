49887e0334eaa6813ed420623d7468bccc89bcda
Kirill <k.smirnov@hh.ru>
2023-05-22 13:32:48 +0300

    fix sync

--- config_sync.sh
+++ config_sync.sh
@@ -3,37 +3,37 @@
 set -ex
 
 function config_sync {
-  cp "$1/configs/.bashrc" "~/.bashrc"
-  cp "$1/configs/.xinitrc" "~/.xinitrc"
-  cp "$1/configs/.bash_history" "~/.bash_history"
+  cp "$1/configs/.bashrc" "/home/$USER/.bashrc"
+  cp "$1/configs/.xinitrc" "/home/$USER/.xinitrc"
+  cp "$1/configs/.bash_history" "/home/$USER/.bash_history"
 
-  cp "$1/configs/desktop-items-0.conf" "~/.config/pcmanfm/default/desktop-items-0.conf"
-  substitute_variables "~/.config/pcmanfm/default/desktop-items-0.conf"
+  cp "$1/configs/desktop-items-0.conf" "/home/$USER/.config/pcmanfm/default/desktop-items-0.conf"
+  substitute_variables "/home/$USER/.config/pcmanfm/default/desktop-items-0.conf"
 
-  cp /etc/xdg/openbox "~/.config" -r
-  cp "$1/configs/autostart" "~/.config/openbox/autostart"
-  cp "$1/configs/rc.xml" "~/.config/openbox/rc.xml"
-  substitute_variables "~/.config/openbox/autostart"
-  substitute_variables "~/.config/openbox/rc.xml"
+  cp /etc/xdg/openbox "/home/$USER/.config" -r
+  cp "$1/configs/autostart" "/home/$USER/.config/openbox/autostart"
+  cp "$1/configs/rc.xml" "/home/$USER/.config/openbox/rc.xml"
+  substitute_variables "/home/$USER/.config/openbox/autostart"
+  substitute_variables "/home/$USER/.config/openbox/rc.xml"
 
-  cp "$1/configs/.Xresources" "~/.Xresources"
-  substitute_variables "~/.Xresources"
+  cp "$1/configs/.Xresources" "/home/$USER/.Xresources"
+  substitute_variables "/home/$USER/.Xresources"
 
-  cp "$1/configs/.gtkrc-2.0" "~/.gtkrc-2.0"
-  substitute_variables "~/.gtkrc-2.0"
+  cp "$1/configs/.gtkrc-2.0" "/home/$USER/.gtkrc-2.0"
+  substitute_variables "/home/$USER/.gtkrc-2.0"
 
-  cp "$1/configs/libfm.conf" "~/.config/libfm/libfm.conf"
-  substitute_variables "~/.config/libfm/libfm.conf"
+  cp "$1/configs/libfm.conf" "/home/$USER/.config/libfm/libfm.conf"
+  substitute_variables "/home/$USER/.config/libfm/libfm.conf"
 
-  cp "$1/configs/panel" "~/.config/lxpanel/default/panels/panel"
-  substitute_variables "~/.config/lxpanel/default/panels/panel"
+  cp "$1/configs/panel" "/home/$USER/.config/lxpanel/default/panels/panel"
+  substitute_variables "/home/$USER/.config/lxpanel/default/panels/panel"
 
-  rm -f "~/Desktop/"*.desktop
-  cp "$1/configs/desktop/"* "~/Desktop/"
-  substitute_variables "~/Desktop/idea.desktop"
+  rm -f "/home/$USER/Desktop/"*.desktop
+  cp "$1/configs/desktop/"* "/home/$USER/Desktop/"
+  substitute_variables "/home/$USER/Desktop/idea.desktop"
 
-  cp "$1/configs/.vimrc" "~/.vimrc"
-  substitute_variables "~/.vimrc"
+  cp "$1/configs/.vimrc" "/home/$USER/.vimrc"
+  substitute_variables "/home/$USER/.vimrc"
 
-  cp "$1/configs/.xbindkeysrc" "~/.xbindkeysrc"
+  cp "$1/configs/.xbindkeysrc" "/home/$USER/.xbindkeysrc"
 }
