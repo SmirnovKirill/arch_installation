42bd31ac1652648da2e8632e2d73306356b20c4f
Kirill <k.smirnov@hh.ru>
2025-12-07 15:13:03 +0300

    keyring

--- auto_installation_before_reboot.sh
+++ auto_installation_before_reboot.sh
@@ -228,7 +228,7 @@ udevadm trigger #код выше чтобы часы распознавалис�
 sed -i 's/hosts: mymachines resolve/hosts: mymachines mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf #чтобы принтер искался
 sed -i 's/Listen \/run\/cups\/cups.sock/#Listen \/run\/cups\/cups.sock/g' /etc/cups/cupsd.conf #чтобы не было бесконечного ожидания принтера при печати
 
-sed -i '/^auth[[:space:]]\+include[[:space:]]\+system-local-login[[:space:]]*$/a auth       optional   pam_gnome_keyring.so' /etc/pam.d/login
-sed -i '/^session[[:space:]]\+include[[:space:]]\+system-local-login[[:space:]]*$/a session    optional   pam_gnome_keyring.so auto_start' /etc/pam.d/login
+sed -i '/^auth[[:space:]]\+include[[:space:]]\+system-local-login[[:space:]]*$/a auth       optional     pam_gnome_keyring.so' /etc/pam.d/login
+sed -i '/^session[[:space:]]\+include[[:space:]]\+system-local-login[[:space:]]*$/a session    optional     pam_gnome_keyring.so auto_start' /etc/pam.d/login
 
 sudo -u "$USER" git clone "$REPOSITORY_ROOT_URL/arch_installation.git" "/home/$USER/arch_installation"
